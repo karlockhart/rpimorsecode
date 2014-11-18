75cba6e41d370584ab084eaad6799eff71a6cfff
Karl Lockhart <klockhart@isightpartners.com>
2014-11-17 22:01:13 -0600

    Added punctuation and as a result had to abstract out field separators

--- src/co/zer0/raspberrypi/rpimorsecode/morsecodeelements/MorseCodeTranslationTable.java
+++ src/co/zer0/raspberrypi/rpimorsecode/morsecodeelements/MorseCodeTranslationTable.java
@@ -27,6 +27,9 @@ public class MorseCodeTranslationTable {
 
     public static final long INTER_LETTER_MULTIPLIER = 3;
     public static final char WORD_SEPARATOR = ' ';
+    public static final char CODE_FILE_SEPARATOR = '|';
+    public static final char CODE_FILE_DOT = '.';
+    public static final char CODE_FILE_DASH = '-';
 
     static final Logger logger = Logger.getLogger(MorseCodeTranslationTable.class.getName());
 
@@ -42,7 +45,8 @@ public class MorseCodeTranslationTable {
         String input;
 
         while ((input = br.readLine()) != null) {
-            String[] parts = input.split(",");
+            String codeFileSeparator = new Character(CODE_FILE_SEPARATOR).toString();
+            String[] parts = input.split(codeFileSeparator);
 
             PulseSequence sequence = new PulseSequence();
 
@@ -57,11 +61,11 @@ public class MorseCodeTranslationTable {
                     } catch (NumberFormatException e) { /* NOP */ }
                     continue;
                 }
-
+                
                 for (int current = 0; current < parts[1].length(); current++) {
-                    if (parts[1].charAt(current) == '.') {
+                    if (parts[1].charAt(current) == CODE_FILE_DOT) {
                         sequence.add(dot);
-                    } else if (parts[1].charAt(current) == '-') {
+                    } else if (parts[1].charAt(current) == CODE_FILE_DASH) {
                         sequence.add(dash);
                     }
                 }
@@ -76,17 +80,23 @@ public class MorseCodeTranslationTable {
 
     }
 
-    public void play(GpioPinDigitalOutput pin, String string) {
+    public void play(GpioPinDigitalOutput pin, String inputString) {
         try {
-            if (string != null) {
-                for (int current = 0; current < string.length(); current++) {
-                    Character character = string.toLowerCase().charAt(current);
+
+            String separator = new Character(WORD_SEPARATOR).toString();
+
+            //Clean the input string
+            String cleanedString = this.cleanInputString(inputString);
+            
+            if (cleanedString != null) {
+                for (int current = 0; current < cleanedString.length(); current++) {
+                    Character character = cleanedString.toLowerCase().charAt(current);
                     if (characterMap.containsKey(character.toString())) {
                         characterMap.get(character.toString()).play(pin);
                         //If there is at least one character left
-                        if (current + 1 < string.length()
+                        if (current + 1 < cleanedString.length()
                                 && //And it isn't the word separator
-                                string.charAt(current + 1) != WORD_SEPARATOR) {
+                                cleanedString.charAt(current + 1) != WORD_SEPARATOR) {
                             Thread.sleep(TimingScheme.getInstance().getGlobalTimingUnit()
                                     * INTER_LETTER_MULTIPLIER);
                         }
@@ -96,5 +106,20 @@ public class MorseCodeTranslationTable {
         } catch (Exception ex) {
             Logger.getLogger(MorseCodeTranslationTable.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    
+    private String cleanInputString(String input){
+        
+        StringBuilder cleanedString = new StringBuilder();
+        
+        for (int current = 0; current < input.length(); current++)
+        {
+            String thisCharacter = new Character(input.charAt(current)).toString();
+            if (this.characterMap.containsKey(thisCharacter)){
+                cleanedString.append(thisCharacter);
+            }
+        }
+        
+        return cleanedString.toString();
     }
 }
